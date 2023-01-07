b6c01e31438549a2f65d4b20f8217930888641e5
Alexey Leping <aleping@yandex.ru>
2023-01-08 01:27:24 +0300

    final

--- src/main/java/com/example/news/controllers/GlobalControllerExceptionHandler.java
+++ src/main/java/com/example/news/controllers/GlobalControllerExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.example.news.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,10 +10,11 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHandler {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
     @ExceptionHandler(value = { Exception.class })
-    public ResponseEntity<String> handle() {
-        String message = "Неправильный запрос, попробуйте по другому!";
-        return ResponseEntity.internalServerError().body(message);
+    public ResponseEntity<String> handle(HttpServletRequest request, Exception exception) {
+        LOGGER.error(String.format("INTERNAL_SERVER_ERROR", request.getRequestURI(), request.getParameterMap()), exception);
+        return ResponseEntity.internalServerError()
+                .body(String.format("Некорректный запрос, попробуйте по другому!", exception.getMessage()));
     }
 }
